d04b5d4d52836329fe693c4766e0fef1a4959651
kavya-83175 <kavyanulu@gmail.com>
2025-07-18 05:53:37 +0530

    Add files via upload

---
+++ BinarySearchTree.h
@@ -0,0 +1,54 @@
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+#include<iostream>
+using namespace std;
+struct TreeNode
+{
+	int data;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int val){
+		data = val;
+		left = right = NULL;
+	}
+};
+
+struct BinarysearchTree
+{
+	TreeNode *root = NULL;
+	TreeNode *ADD(int val,TreeNode *root){
+	    if(root==NULL)return new TreeNode(val);
+		if(val<root->data){
+		root->left = ADD(val,root->left);
+	}
+		else{
+		root->right = ADD(val,root->right);
+	}
+	return root;
+    } 
+    
+    void insert(int val)
+    {
+    	root = ADD(val,root);
+	}
+	
+	void PrintInorder(TreeNode *root)
+	{
+		if(root==NULL)return;
+		PrintInorder(root->left);
+		cout<<root->data<<" ";
+		PrintInorder(root->right);
+	}
+	
+	void Inorder(){
+		PrintInorder(root);
+	}
+};
+#endif
+
+
+
+
+
+
